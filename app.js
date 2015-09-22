9997df1b064f4a616fce8a202b8e5bc902513daa
Sarah Edkins <edkins.sarah@gmail.com>
2015-09-22 17:53:49 -0400

    Swig required

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require( 'express' );
 var morgan = require('morgan');
+var swig = require('swig');
 var app = express(); // creates an instance of an express application
 
 app.use(function (req, res, next){
@@ -16,5 +17,21 @@ app.get('/', function(req, res){
 app.get('/news', function(req, res){
   res.send("This is the news!");
 });
+
+// DATA FOR SWIG
+var data = {
+    title: 'An Example',
+    people: [{
+        name: 'Gandalf',
+    }, {
+        name: 'Frodo'
+    }, {
+        name: 'Hermione'
+    }]
+};
+swig.renderFile(__dirname + '/views/index.html', data, function (err, output) {
+    console.log(output);
+});
+
 
 app.listen(8080, console.log('server listening'));
